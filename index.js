ea044ef46c9a4d5499d9332b88205f935c2aea07
donatassiau <donatas.siaud@gmail.com>
2024-08-27 16:33:04 +0300

    Create Account

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-import express from 'express';
-import { pageHome } from './pages/pageHome.js';
-import { page404 } from './pages/page404.js';
-import { pageAbout } from './pages/pageAbout.js';
+import express from "express";
+import { pageHome } from "./pages/pageHome.js";
+import { page404 } from "./pages/page404.js";
+import { pageAbout } from "./pages/pageAbout.js";
+import { pageViewAllAccounts } from "./pages/pageViewAllAccounts.js";
+import { pageCreateAccount } from "./pages/pageCreateAccount.js";
 
 const app = express();
 const port = 5018;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    return res.send(pageHome());
+app.get("/", (req, res) => {
+  return res.send(pageHome());
 });
 
-app.get('/about', (req, res) => {
-    return res.send(pageAbout());
+app.get("/about", (req, res) => {
+  return res.send(pageAbout());
 });
 
-// app.get('/css/main.css', (req, res) => {
-//     return res
-//         .setHeader('Content-Type', 'text/css')
-//         .send(`* {margin: 0;}`);
-// });
+app.get("/create-account", (req, res) => {
+  return res.send(pageCreateAccount());
+});
 
-app.get('*', (req, res) => {
-    return res.send(page404());
+app.get("/accounts", (req, res) => {
+  return res.send(pageViewAllAccounts());
+});
+
+app.get("*", (req, res) => {
+  return res.send(page404());
+});
+
+app.use((req, res, next) => {
+  return res.status(404).send("Sorry can't find that!");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  return res.status(500).send("Something broke!");
 });
 
 app.listen(port, () => {
-    console.log(`Serveris pasileido: http://localhost:${port}`);
+  console.log(`Serveris pasileido: http://localhost:${port}`);
 });
